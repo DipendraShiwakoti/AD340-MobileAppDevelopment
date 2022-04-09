bec86a2c0145aa3dbc241bff4452d3c76ec2e17d
DipendraShiwakoti <dipendra.shiwakoti@go.shoreline.edu>
2022-04-09 16:54:49 -0700

    first commit in hello_world project

---
+++ app/src/main/java/com/example/ad_340dipendraweek1assignment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ad_340dipendraweek1assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
