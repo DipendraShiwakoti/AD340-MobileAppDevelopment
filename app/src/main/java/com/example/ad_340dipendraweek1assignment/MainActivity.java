d0332510f4932127761ed0f021bd4abde3610443
DipendraShiwakoti <dipendra.shiwakoti@go.shoreline.edu>
2022-05-07 15:42:42 -0700

    Assignment 2 resubmit

--- app/src/main/java/com/example/ad_340dipendraweek1assignment/MainActivity.java
+++ app/src/main/java/com/example/ad_340dipendraweek1assignment/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
@@ -19,20 +18,18 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.Calendar;
 
- public class MainActivity extends AppCompatActivity
-         implements DatePickerDialog.OnDateSetListener{
+public class MainActivity extends AppCompatActivity
+        implements DatePickerDialog.OnDateSetListener {
+
     private EditText nameField;
     private EditText emailAddressField;
-    private EditText userNameField;
+    private EditText usernameField;
     private TextView dobTextView;
-    private EditText descriptionField;
-    private EditText occupationField;
-
     private int dobYear = 0;
     private int dobMonth = 0;
     private int dobDay = 0;
 
-    @SuppressLint("WrongViewCast")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,21 +37,22 @@ import java.util.Calendar;
 
         nameField = findViewById(R.id.nameField);
         emailAddressField = findViewById(R.id.emailAddress);
-        userNameField = findViewById(R.id.userName);
-        dobTextView = findViewById(R.id.nameAndDate);
-        descriptionField = findViewById(R.id.descriptionField);
-        occupationField = findViewById(R.id.occupationField);
+        usernameField = findViewById(R.id.username);
+        dobTextView = findViewById(R.id.selectedDateOfBirth);
     }
-     public void onSubmit(View view){
+
+
+    public void onSubmit(View view) {
         String name = nameField.getText().toString();
         String emailAddress = emailAddressField.getText().toString();
-        String username = userNameField.getText().toString();
+        String username = usernameField.getText().toString();
 
-
-        if(name.equals("") || emailAddress.equals("") || userNameField.equals("") || dobYear == 0 || dobMonth == 0 || dobDay == 0){
-            Toast.makeText(getApplicationContext(), getString(R.string.forget_date_of_Birth), Toast.LENGTH_LONG).show();
+        if (name.equals("") || emailAddress.equals("") || username.equals("") || dobYear == 0 || dobMonth == 0 || dobDay == 0) {
+            // empty strings are not valid form input show a Toast to the user
+            Toast.makeText(getApplicationContext(), R.string.Date_Of_birth, Toast.LENGTH_LONG).show();
             return;
         }
+
         if (!Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()) {
             Toast.makeText(getApplicationContext(), getString(R.string.email_address_error), Toast.LENGTH_LONG).show();
             return;
@@ -65,19 +63,45 @@ import java.util.Calendar;
         int years = Period.between(dateOfBirth, currentDate).getYears();
 
         if (years < 18) {
-            Toast.makeText(getApplicationContext(),getString(R.string.Eighteen_years_error), Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), R.string.Eighteen_years, Toast.LENGTH_LONG).show();
             return;
         }
 
         Intent intent = new Intent(getApplicationContext(), WelcomeScreen.class);
-        intent.putExtra(Constants.Username_Key, username);
+        intent.putExtra(Constants.USERNAME_KEY, username);
         startActivity(intent);
     }
+
     public void onDobClick(View view) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
+     @Override
+     public void onSaveInstanceState(@NonNull Bundle outState) {
+         super.onSaveInstanceState(outState);
+         outState.putString(Constants.SELECTED_DOB, dobTextView.getText().toString());
+     }
+
+     @Override
+     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+         super.onRestoreInstanceState(savedInstanceState);
+         if (savedInstanceState.containsKey(Constants.SELECTED_DOB)) {
+             dobTextView.setText(savedInstanceState.getString(Constants.SELECTED_DOB));
+         }
+     }
+
+     @Override
+     protected void onRestart() {
+         super.onRestart();
+         nameField.setText("");
+         usernameField.setText("");
+         emailAddressField.setText("");
+         dobTextView.setText("");
+         dobYear = 0;
+         dobDay = 0;
+         dobMonth = 0;
+     }
 
      public void onDateSet(DatePicker view, int year, int month, int day) {
          month = month + 1;
@@ -95,10 +119,8 @@ import java.util.Calendar;
             int year = c.get(Calendar.YEAR);
             int month = c.get(Calendar.MONTH);
             int day = c.get(Calendar.DAY_OF_MONTH);
-
-            return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+            return new DatePickerDialog(getActivity(), (MainActivity) getActivity(), year, month, day);
         }
     }
-
 }
 
--- app/src/main/java/com/example/ad_340dipendraweek1assignment/WelcomeScreen.java
+++ app/src/main/java/com/example/ad_340dipendraweek1assignment/WelcomeScreen.java
@@ -10,31 +10,31 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 
- public class WelcomeScreen extends AppCompatActivity {
+public class WelcomeScreen extends AppCompatActivity {
+
     private TextView welcomeText;
-    
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setConstantView(R.layout.activity_welcomescreen);
-       welcomeText = findViewById(R.id.welcomeText);
-        
+        setContentView(R.layout.activity_welcomescreen);
+        welcomeText = findViewById(R.id.welcomeText);
+
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        
-       if (bundle != null) {
-            if(bundle.containsKey(Constants.Username_Key)){
-                welcomeText.setText(getString(R.string.Thank_You) + bundle.getString(Constants.Username_Key) +"!");
+
+        if (bundle != null) {
+            if (bundle.containsKey(Constants.USERNAME_KEY)) {
+                welcomeText.setText(getString(R.string.Thank_you_message) + bundle.getString(Constants.USERNAME_KEY) +getString(R.string.exclamaion));
             }
         }
-        
     }
 
-     private void setConstantView(int activity_welcomescreen) {
-     }
+    public void onBackClick(View view) {
 
-
- }
+        finish();
+    }
+}
 
 
 
